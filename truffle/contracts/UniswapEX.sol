89d0845fd06f6fd1d0ffa511f81e86caad4f75c3
Agustin Aguilar <agusxrun@gmail.com>
2019-08-24 10:42:42 +0200

    Simplify Uniswap trades

--- truffle/contracts/UniswapEX.sol
+++ truffle/contracts/UniswapEX.sol
@@ -1,15 +1,60 @@
 pragma solidity ^0.5.11;
 
+
+import "./commons/SafeMath.sol";
 import "./interfaces/IERC20.sol";
 import "./interfaces/UniswapExchange.sol";
 import "./interfaces/UniswapFactory.sol";
 
 
 contract UniswapEX {
+    using SafeMath for uint256;
+
     address public constant ETH_ADDRESS = address(0x00eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee);
     uint256 private constant never = uint(-1);
 
     UniswapFactory public uniswapFactory;
+
+    function _ethToToken(
+        UniswapFactory _uniswapFactory,
+        IERC20 _token,
+        uint256 _amount,
+        address _dest
+    ) private returns (uint256) {
+        UniswapExchange uniswap = _uniswapFactory.getExchange(address(_token));
+        if (_dest != address(this)) {
+            return uniswap.ethToTokenTransferInput.value(_amount)(1, never, _dest);
+        } else {
+            return uniswap.ethToTokenSwapInput.value(_amount)(1, never);
+        }
+    }
+
+    function _tokenToEth(
+        UniswapFactory _uniswapFactory,
+        IERC20 _token,
+        uint256 _amount,
+        address _dest
+    ) private returns (uint256) {
+        UniswapExchange uniswap = uniswapFactory.getExchange(address(_token));
+
+        // Check if previues allowance is enought
+        // and approve Uniswap if is not
+        uint256 prevAllowance = _token.allowance(address(this), address(uniswap));
+        if (prevAllowance < _amount) {
+            if (prevAllowance != 0) {
+                _token.approve(address(uniswap), 0);
+            }
+
+            _token.approve(address(uniswap), uint(-1));
+        }
+
+        // Execute the trade
+        if (_dest != address(this)) {
+            uniswap.tokenToEthTransferInput(_amount, 1, never, _dest);
+        } else {
+            uniswap.tokenToEthSwapInput(_amount, 1, never);
+        }
+    }
 
     function create(
         IERC20 _from,
@@ -31,38 +76,26 @@ contract UniswapEX {
     ) external {
         if (address(_from) == ETH_ADDRESS) {
             // Keep some eth for paying the fee
-            uint256 sell = _amount - _fee;
-            // Transfer fee
-            msg.sender.transfer(_fee);
-            // Convert using Uniswap
-            UniswapExchange uniswap = uniswapFactory.getExchange(address(_to));
-            uniswap.ethToTokenTransferInput.value(sell)(_return, never, _owner);
+            uint256 sell = _amount.sub(_fee);
+            uint256 bought = _ethToToken(uniswapFactory, _from, sell, _owner);
+            require(bought >= _return, "sell return is not enought");
+            _owner.transfer(_fee);
         } else if (address(_to) == ETH_ADDRESS) {
-            // Add extra fee
-            uint256 buy = _return + _fee;
-
             // Convert
-            UniswapExchange uniswap = uniswapFactory.getExchange(address(_from));
-            _from.approve(address(uniswap), never);
-            uint256 bought = uniswap.tokenToEthSwapInput(_amount, buy, never);
+            uint256 bought = _tokenToEth(uniswapFactory, _to, _amount, address(this));
+            require(bought >= _return.add(_fee), "sell return is not enought");
 
             // Send fee and amount bought
             msg.sender.transfer(_fee);
-            _owner.transfer(bought - _fee);
+            _owner.transfer(bought.sub(_fee));
         } else {
-            // Separate the two steps
-
-            // Convert to ETH
-            UniswapExchange uniswap = uniswapFactory.getExchange(address(_from));
-            _from.approve(address(uniswap), uint(-1));
-            uint256 bought = uniswap.tokenToEthSwapInput(_amount, 1, never);
-
-            // Send fee and amount bought
+            // Convert from FromToken to ETH
+            uint256 boughtEth = _tokenToEth(uniswapFactory, _from, _amount, address(this));
             msg.sender.transfer(_fee);
-            
-            // Convert to final token
-            UniswapExchange uniswapb = uniswapFactory.getExchange(address(_to));
-            uniswapb.ethToTokenTransferInput.value(bought - _fee)(_return, never, _owner);
+
+            // Convert from ETH to ToToken
+            uint256 boughtToken = _ethToToken(uniswapFactory, _to, boughtEth.sub(_fee), _owner);
+            require(boughtToken >= _return, "sell return is not enought");
         }
     }
 
