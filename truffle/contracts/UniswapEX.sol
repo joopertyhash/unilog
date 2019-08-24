740aa1a3991a69563159fddcc5992202a18d1dba
Agustin Aguilar <agusxrun@gmail.com>
2019-08-24 11:05:19 +0200

    Add ETH Deposits

--- truffle/contracts/UniswapEX.sol
+++ truffle/contracts/UniswapEX.sol
@@ -10,10 +10,14 @@ import "./interfaces/UniswapFactory.sol";
 contract UniswapEX {
     using SafeMath for uint256;
 
+    event DepositETH(uint256 _amount, bytes _data);
+
     address public constant ETH_ADDRESS = address(0x00eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee);
     uint256 private constant never = uint(-1);
 
     UniswapFactory public uniswapFactory;
+
+    mapping(bytes32 => uint256) public ethDeposits;
 
     function _ethToToken(
         UniswapFactory _uniswapFactory,
@@ -56,33 +60,70 @@ contract UniswapEX {
         }
     }
 
-    function create(
+    function _pull(
+        IERC20 _from,
+        bytes32 _key
+    ) private returns (uint256 amount) {
+        if (address(_from) == ETH_ADDRESS) {
+            amount = ethDeposits[_key];
+            ethDeposits[_key] = 0;
+        } else {
+            // TODO: pull tokens from Fabric lib
+            revert("not implemented");
+        }
+    }
+
+    function _keyOf(
         IERC20 _from,
         IERC20 _to,
-        uint256 _amount,
         uint256 _return,
-        uint256 _fee
-    ) external {
-        
+        uint256 _fee,
+        address payable _owner
+    ) private returns (bytes32) {
+        return keccak256(abi.encodePacked(
+            _from,
+            _to,
+            _return,
+            _fee,
+            _owner
+        ));
+    }
+
+    function depositETH(
+        bytes calldata _data
+    ) external payable {
+        bytes32 key = keccak256(_data);
+        ethDeposits[key] = ethDeposits[key].add(msg.value);
+        emit DepositETH(msg.value, _data);
     }
 
     function execute(
         IERC20 _from,
         IERC20 _to,
-        uint256 _amount,
         uint256 _return,
         uint256 _fee,
         address payable _owner
     ) external {
+        bytes32 key = _keyOf(
+            _from,
+            _to,
+            _return,
+            _fee,
+            _owner
+        );
+
+        // Pull amount
+        uint256 amount = _pull(_from, key);
+
         if (address(_from) == ETH_ADDRESS) {
             // Keep some eth for paying the fee
-            uint256 sell = _amount.sub(_fee);
+            uint256 sell = amount.sub(_fee);
             uint256 bought = _ethToToken(uniswapFactory, _from, sell, _owner);
             require(bought >= _return, "sell return is not enought");
             _owner.transfer(_fee);
         } else if (address(_to) == ETH_ADDRESS) {
             // Convert
-            uint256 bought = _tokenToEth(uniswapFactory, _to, _amount, address(this));
+            uint256 bought = _tokenToEth(uniswapFactory, _to, amount, address(this));
             require(bought >= _return.add(_fee), "sell return is not enought");
 
             // Send fee and amount bought
@@ -90,7 +131,7 @@ contract UniswapEX {
             _owner.transfer(bought.sub(_fee));
         } else {
             // Convert from FromToken to ETH
-            uint256 boughtEth = _tokenToEth(uniswapFactory, _from, _amount, address(this));
+            uint256 boughtEth = _tokenToEth(uniswapFactory, _from, amount, address(this));
             msg.sender.transfer(_fee);
 
             // Convert from ETH to ToToken
